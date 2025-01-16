336762fef9ebae86fd80ad773eb1375f1e2f4ceb
radagesth <49539151+radagesth@users.noreply.github.com>
2025-01-16 12:24:54 -0300

    productos actualizados

--- Page/mielesrocoweb/src/data/products.js
+++ Page/mielesrocoweb/src/data/products.js
@@ -10,12 +10,5 @@ export const products = [
       price: 7000,
       image: "https://images.unsplash.com/photo-1471943311424-646960669fbc?auto=format&fit=crop&w=800&q=80",
       description: "1 KG de Miel pura y natural recolectada de flores silvestres ubicadas en la Región del Maule de la Provincia de Linares."
-    },
-    {
-      id: 2,
-      name: "Miel de Flores Silvestres 250GR",
-      price: 2500,
-      image: "https://images.unsplash.com/photo-1471943311424-646960669fbc?auto=format&fit=crop&w=800&q=80",
-      description: "250 GR de Miel pura y natural recolectada de flores silvestres ubicadas en la Región del Maule de la Provincia de Linares."
     }
   ];
